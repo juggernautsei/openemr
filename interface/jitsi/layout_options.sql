540793177b6cb04b1045f9f9230acd9def374563
Sherwin <sherwingaddis@gmail.com>
2021-03-20 02:47:57 -0400

    SQL file for the demographics

---
+++ interface/jitsi/layout_options.sql
@@ -0,0 +1,5 @@
+
+REPLACE INTO `layout_options` (`form_id`, `field_id`, `group_id`, `title`, `seq`, `data_type`, `uor`, `fld_length`, `max_length`, `list_id`, `titlecols`, `datacols`, `default_value`, `edit_options`, `description`, `fld_rows`, `list_backup_id`, `source`, `conditions`, `validation`) VALUES
+('DEM', 'genericval2', '1', '', 20, 31, 1, 0, 255, '', 1, 3, '', '', '<div id=\'tele\' style=\'float: right\'><button id=\'telehealth\' class=\'btn btn-primary\'>Telehealth</button></div>', 0, '', 'F', '', '');
+
+
